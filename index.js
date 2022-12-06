bdb818c70ad140640005d41541547162210c2f3b
Shamil Irfan <shamilirfan28@gmail.com>
2022-12-05 21:43:46 -0500

    2ndHome Work

--- index.js
+++ index.js
@@ -25,54 +25,6 @@ app.post("/add-dress", (req, res) => {
         res.send(`${req.body.name} added`);
     });
 });
-app.get("/color", (req, res) => {
-    fs.readFile("database", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        res.send(JSON.stringify(allData.color));
-    });
-});
-app.post("/add-color", (req, res) => {
-    fs.readFile("database", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        const colorData = req.body;
-        colorData.id = allData.color.length + 1;
-        allData.color.push(colorData);
-        fs.writeFile("database", JSON.stringify(allData), () => { });
-        res.send(`${req.body.name} added`);
-    });
-});
-app.get("/size", (req, res) => {
-    fs.readFile("database", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        res.send(JSON.stringify(allData.size));
-    });
-});
-app.post("/add-size", (req, res) => {
-    fs.readFile("database", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        const sizeData = req.body;
-        sizeData.id = allData.size.length + 1;
-        allData.size.push(sizeData);
-        fs.writeFile("database", JSON.stringify(allData), () => { });
-        res.send(`${req.body.name} added`);
-    });
-});
-app.get("/brand", (req, res) => {
-    fs.readFile("database", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        res.send(JSON.stringify(allData.brand));
-    });
-});
-app.post("/add-brand", (req, res) => {
-    fs.readFile("database", "utf8", (err, data) => {
-        const allData = JSON.parse(data);
-        const brandData = req.body;
-        brandData.id = allData.brand.length + 1;
-        allData.brand.push(brandData);
-        fs.writeFile("database", JSON.stringify(allData), () => { });
-        res.send(`${req.body.name} added`);
-    });
-});
 
 
 app.listen(port, () => {
